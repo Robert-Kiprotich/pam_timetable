d85151bed148844ee3892e25ac12e921bab425c5
hospitalkkt <89789200+hospitalkkt@users.noreply.github.com>
2021-11-24 16:00:51 +0300

    final commit

---
+++ home-page.php
@@ -0,0 +1,382 @@
+<?php
+session_start()
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet"> -->
+
+    <title>Timetable</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+
+
+
+  </head>
+
+  <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <div class="sub-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-xs-12">
+            <ul class="left-info">
+              <li><a href="#"><i class="fa fa-clock-o"></i>Mon-Fri 09:00-17:00</a></li>
+              <li><a href="#"><i class="fa fa-phone"></i>254 701 518237</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <ul class="right-icons">
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+              <li><a href="#"><i class="fa fa-behance"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <header class="">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="index.html"><h2> Online Timetable Management System</h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link current" href="#top">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="#about">About Us</a>
+              </li>    
+              <li class="nav-item">
+                <a class="nav-link" href="#services">Services</a>
+              </li>          
+              <li class="nav-item">
+                <a class="nav-link" href="#contactus">Contact Us</a>
+              </li>
+              
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Page Content -->
+    <!-- Banner Starts Here -->
+    <div class="main-banner header-text" id="top">
+        <div class="Modern-Slider">
+          <!-- Item -->
+          <div class="item item-1">
+            <div class="img-fill">
+                <div class="text-content">
+                  <h6>we are ready to help you in</h6>
+                  <h4>Class Timetable<br> management</h4>
+                  <p>Create,view class Timetable You can download as excel spreadsheet</p>
+                  <a href="index.php" class="filled-button">Login&signup</a>
+                </div>
+            </div>
+          </div>
+          <!-- // Item -->
+          <!-- Item -->
+          <div class="item item-2">
+            <div class="img-fill">
+                <div class="text-content">
+                  <h6>we are here to support you in</h6>
+                  <h4>Exam timetable<br>&amp; Management</h4>
+                  <p>Create,view class Timetable You can download as excel spreadsheet</p>
+                  <a href="index.php" class="filled-button">Login&signup</a>
+                </div>
+            </div>
+          </div>
+          <!-- // Item -->
+          <!-- Item -->
+          <div class="item item-3">
+            <div class="img-fill">
+                <div class="text-content">
+                  <h6>we make sure you get</h6>
+                  <h4>School anouncements<br></h4>
+                  <p>Create,view class Timetable You can download as excel spreadsheet</p>
+                  <a href="index.php" class="filled-button">Login&signup</a>
+                </div>
+            </div>
+          </div>
+          <!-- // Item -->
+        </div>
+    </div>
+    <!-- Banner Ends Here -->
+
+    <div class="request-form">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h4>Request a call back right now ?</h4>
+            <span>Mauris ut dapibus velit cras interdum nisl ac urna tempor mollis.</span>
+          </div>
+         
+        </div>
+      </div>
+    </div>
+    <div class="more-info" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="more-info-content">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="left-image">
+                    <img src="assets/images/pic7.jpg" alt="">
+                  </div>
+                </div>
+                <div class="col-md-6 align-self-center">
+                  <div class="right-content">
+                    <span>Who we are</span>
+                    <h2>Get to know about <em>Us</em></h2>
+                    <p>Curabitur pulvinar sem a leo tempus facilisis. Sed non sagittis neque. Nulla conse quat tellus nibh, id molestie felis sagittis ut. Nam ullamcorper tempus ipsum in cursus<br><br>Praes end at dictum metus. Morbi id hendrerit lectus, nec dapibus ex. Etiam ipsum quam, luctus eu egestas eget, tincidunt</p>
+                    <a href="index.php" class="filled-button">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="container" id="services">
+    <div class="card-deck">
+      <div class="card text-center">
+      <img class="card-img-top" src="assets/images/school.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title"><strong>Class Timetable</strong></h5>
+    <p class="card-text">Click here to view class timetable
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+    </p>
+    <a href="index.php" class="btn " style="background:#a4c639; color:white;">View</a>
+        </div>
+      </div>
+      <div class="card text-center">
+      <img class="card-img-top" src="assets/images/university.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title"><strong>Exam Timetable</strong></h5>
+    <p class="card-text">Click here to view Exam timetable
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+    </p>
+    <a href="index.php" class="btn " style="background:#a4c639; color:white;">View</a>
+        </div>
+      </div>
+      <div class="card text-center">
+      <img class="card-img-top" src="assets/images/university (2).png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title"><strong>Personal Timetable</strong></h5>
+    <p class="card-text">Click here to view your personal timetable
+      <br>
+      <br>
+      <br>
+      <br>
+     
+    </p>
+    <a href="index.php" class="btn " style="background:#a4c639; color:white;">View</a>
+        </div>
+      </div>
+      <div class="card text-center">
+      <img class="card-img-top" src="assets/images/university (1).png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title"><strong>Notices</strong></h5>
+    <p class="card-text">Click here to view  notices
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+    </p>
+    <a href="index.php" class="btn " style="background:#a4c639; color:white;">View</a>
+        </div>
+      </div>
+      <div class="card text-center">
+      <img class="card-img-top" src="assets/images/college.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title"><strong>Departments</strong></h5>
+    <p class="card-text">Click here to view your respective departments
+      <br>
+      <br>
+      <br>
+      <br>
+     
+     
+    </p>
+    <a href="index.php" class="btn " style="background:#a4c639; color:white;">View</a>
+        </div>
+      </div>
+      <div class="card  text-center">
+      <img class="card-img-top" src="assets/images/education.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title"><strong>Students Details</strong></h5>
+    <p class="card-text">Click here to view student  details
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+    </p>
+    <a href="index.php" class="btn " style="background:#a4c639; color:white; align-items:center;">View</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+    <br>
+    <br>
+    <br>
+
+    <!-- Footer Starts Here -->
+    <footer>
+      <div class="container" id="contactus">
+        <div class="row">
+          <div class="col-md-6 footer-item">
+            <h4>Social</h4>
+            <p>Find us on social media by clicking icons below.</p>
+            <ul class="social-icons">
+              <li><a rel="nofollow" href="https://fb.com/templatemo" target="_blank"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+              <li><a href="#"><i class="fa fa-behance"></i></a></li>
+            </ul>
+          </div>
+         
+          <div class="col-md-3 footer-item">
+            <h4>Contacts</h4>
+            <ul class="menu-list">
+              <li><a href="#"><i class="fa fa-phone">Safaricom:+25472252525</i></a></li>
+              <li><a href="#"><i class="fa fa-phone">Airtel:+2548776856</i></a></li>
+              <li><a href="#"><i class="fa fa-whatsapp">Whatsapp:072323234</i></a></li>
+              
+            </ul>
+          </div>
+          <div class="col-md-3 footer-item last-item">
+            <h4>Contact Us</h4>
+            <div class="contact-form">
+              <form id="contact footer-contact" action="" method="post">
+                <div class="row">
+                  <div class="col-lg-12 col-md-12 col-sm-12">
+                    <fieldset>
+                      <input name="name" type="text" class="form-control" id="name" placeholder="Full Name" required="">
+                    </fieldset>
+                  </div>
+                  <div class="col-lg-12 col-md-12 col-sm-12">
+                    <fieldset>
+                      <input name="email" type="text" class="form-control" id="email" pattern="[^ @]*@[^ @]*" placeholder="E-Mail Address" required="">
+                    </fieldset>
+                  </div>
+                  <div class="col-lg-12">
+                    <fieldset>
+                      <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your Message" required=""></textarea>
+                    </fieldset>
+                  </div>
+                  <div class="col-lg-12">
+                    <fieldset>
+                      <button type="submit" id="form-submit" class="filled-button">Send Message</button>
+                    </fieldset>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+    <div class="sub-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <p>Copyright &copy; 2021 Online Tmetable management system by (Pamela  Maghuwa)
+            
+      
+          </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/jquery.singlePageNav.min.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/accordions.js"></script>
+    <script>
+      $(function() {
+        // Single Page Nav
+        $('#navbarResponsive').singlePageNav({
+          'offset': 100,
+          'filter': ':not(.external)'
+        });
+
+        // On mobile, close the menu after nav-link click
+        $('#navbarResponsive .navbar-nav .nav-item .nav-link').click(function(){
+          $('#navbarResponsive').removeClass('show');
+        });
+      });
+    </script>
+
+    <script language = "text/Javascript"> 
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+          }
+      }
+    </script>
+
+  </body>
+</html>
